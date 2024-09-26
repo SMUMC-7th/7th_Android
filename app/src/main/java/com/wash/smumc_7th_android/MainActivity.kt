d9102b50d6204fe22e8ec3b61a92bf4b0e72c0ee
yang dong won <didehddnjs89@gmail.com>
2024-09-26 13:53:14 +0900

    design: MainActivity ui 추가

---
+++ app/src/main/java/com/wash/smumc_7th_android/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.wash.smumc_7th_android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.wash.smumc_7th_android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        initView()
+        setNavigation()
+    }
+
+    private fun initView() = with(binding){
+        fun initHomeFragment(){
+            setFragment(HomeFragment())
+        }
+
+        initHomeFragment()
+    }
+
+
+    private fun setNavigation() = with(binding){
+        botNavmain.setOnItemSelectedListener { item ->
+            when(item.itemId){
+               R.id.nav_main_home ->{
+                   setFragment(HomeFragment())
+                   true
+               }
+               R.id.nav_main_profile ->{
+                   true
+               }
+               R.id.nav_main_schedule -> {
+                   true
+               }
+               R.id.nav_main_write ->{
+                   true
+               }
+               else ->false
+            }
+        }
+    }
+
+    private fun setFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView_main, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
