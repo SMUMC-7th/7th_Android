e0024bd30813e1f078067fd18183200619389966
calla20031 <calla20031@naver.com>
2024-09-30 19:11:01 +0900

    commit

--- app/src/main/java/com/wash/smumc_7th_android/MainActivity.kt
+++ app/src/main/java/com/wash/smumc_7th_android/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.wash.smumc_7th_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageButton1 = findViewById<ImageButton>(R.id.imageButton1)
+        val imageButton2 = findViewById<ImageButton>(R.id.imageButton2)
+        val imageButton3 = findViewById<ImageButton>(R.id.imageButton3)
+        val imageButton4 = findViewById<ImageButton>(R.id.imageButton4)
+        val imageButton5 = findViewById<ImageButton>(R.id.imageButton5)
+        val previous = findViewById<ImageButton>(R.id.pervious)
+
+        imageButton1.setOnClickListener {
+            val intent = Intent(this, Page1Activity::class.java)
+            startActivity(intent)
+        }
+        imageButton2.setOnClickListener {
+            val intent = Intent(this, Page2Activity::class.java)
+            startActivity(intent)
+        }
+        imageButton3.setOnClickListener {
+            val intent = Intent(this, Page3Activity::class.java)
+            startActivity(intent)
+        }
+        imageButton4.setOnClickListener {
+            val intent = Intent(this, Page4Activity::class.java)
+            startActivity(intent)
+        }
+        imageButton5.setOnClickListener {
+            val intent = Intent(this, Page5Activity::class.java)
+            startActivity(intent)
+        }
+        previous.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
