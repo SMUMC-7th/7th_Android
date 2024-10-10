d8d405c35bf8d36e026d517ce9619f9426cdfd27
yang dong won <didehddnjs89@gmail.com>
2024-10-11 00:01:26 +0900

    feature: mainActivity, 이미지 파일 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,10 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -41,6 +45,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- app/src/main/java/com/wash/smumc_7th_android/MainActivity.kt
+++ app/src/main/java/com/wash/smumc_7th_android/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.wash.smumc_7th_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.wash.smumc_7th_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initNavigation()
+        initBotNav()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(binding.fragmentContainerViewMain.id) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    private fun initBotNav() {
+        binding.botNavMain.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_main_home -> {
+                    true
+                }
+
+                R.id.menu_main_looker -> {
+                    true
+                }
+
+                R.id.menu_main_audio -> {
+                    true
+                }
+
+                R.id.menu_main_search -> {
+                    true
+                }
+
+                R.id.menu_main_storage -> {
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
